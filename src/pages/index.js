00a0cd9ea282f495ef4e612ce1d68ab8e5caceb1
majing <2292277943@qq.com>
2022-01-08 12:30:21 +0800

    测试

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function() {
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
         <li>111
           <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started  77
+            Getting Started  778
           </a>
         </li>
       </ul>
